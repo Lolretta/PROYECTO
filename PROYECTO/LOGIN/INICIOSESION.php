dc6b34d8e416f1da5801c6caa5fd19f648ca826c
jose.retta <jose.retta@iest.edu.mx>
2024-05-16 01:39:06 -0600

    cambio php

--- PROYECTO/LOGIN/INICIOSESION.php
+++ PROYECTO/LOGIN/INICIOSESION.php
@@ -1,3 +1,7 @@
+<?php 
+    include "dbConn.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
---
+++ dbConn.php
@@ -0,0 +1,8 @@
+<?php
+    $dbhost = "localhost";
+    $dbname = "TacosAna";
+    $dbuname = "root";
+    $dbpass = "isnd";
+
+    $conn = mysqli_connect($dbhost, $dbuname, $dbpass, $dbname);
+?>
